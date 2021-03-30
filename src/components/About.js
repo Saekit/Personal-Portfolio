76e211d7f8523042ac590c5bd04e11fad2fb8e81
Saekit <raquel.sae.randall@gmail.com>
2021-03-29 20:47:14 -0400

    responsive nav, home, and about

--- src/components/About.js
+++ src/components/About.js
@@ -6,45 +6,78 @@ class About extends React.Component {
   }
   render() {
     return (
-      <div className="about-container">
-        <div className="home-about">
-          <img
-            className="selfie"
-            src="/images/portfolio-pic.jpg"
-            alt="selfie"
-          />
-          <p>
-            I am a Flatiron School coding bootcamp alumna! There I learned Ruby
-            and JavaScript programming languages and how to work with others on
-            a team to complete large projects.
-          </p>
-          <h3>Life Before Coding:</h3>
-          <p>
-            I was working full time and taking classes to attend a master’s
-            program for nutrition. After two years, I took a break for family
-            reasons and moved back home for a few months. Back home I wanted to
-            try something new so I opened a pop-up restaurant with a friend and
-            the two of us ran the business everyday, 12 hours a day, for almost
-            6 months. It was exhausting, but a great experience that helped me
-            realize that I can really do anything with the right amount of
-            effort, dedication, and enthusiasm.
-          </p>
-          <p>
-            {" "}
-            It was after this experience that I went back to what I used to like
-            doing as a child, coding. So I started to teach myself how to code
-            using online and offline resources and found that coding felt so
-            right to me. I moved back to Brooklyn and attended the coding
-            bootcamp where I was able to focus on coding, grasp more programming
-            concepts and dive further into my creative abilities while learning
-            to work with others through pair programming.
-          </p>
-          <h3>Life Now:</h3>
-          <p>
-            I'm still living in NYC where I am working hard to continue to improve and grow as a programmer,
-            coding everyday, and loving every learning moment.
-          </p>
-          <p>Live, Love, Code</p>
+      <div className="about-section">
+        <div className="about-container">
+          <div className="selfie-container">
+            <img
+              className="selfie"
+              src="/images/portfolio-pic.jpg"
+              alt="selfie"
+            />
+          </div>
+          <div className="home-about">
+            <p>
+              I am a Flatiron School coding bootcamp alumna! There I learned
+              Ruby and JavaScript programming languages and how to work with
+              others on a team to complete large projects.
+            </p>
+            <h3>Life Before Coding:</h3>
+            <p>
+              I was working full time and taking classes to attend a master’s
+              program for nutrition. After two years, I took a break for family
+              reasons and moved back home for a few months. Back home I wanted
+              to try something new so I opened a pop-up restaurant with a friend
+              and the two of us ran the business everyday, 12 hours a day, for
+              almost 6 months. It was exhausting, but a great experience that
+              helped me realize that I can really do anything with the right
+              amount of effort, dedication, and enthusiasm.
+            </p>
+            <p>
+              {" "}
+              It was after this experience that I went back to what I used to
+              like doing as a child, coding. So I started to teach myself how to
+              code using online and offline resources and found that coding felt
+              so right to me. I moved back to Brooklyn and attended the coding
+              bootcamp where I was able to focus on coding, grasp more
+              programming concepts and dive further into my creative abilities
+              while learning to work with others through pair programming.
+            </p>
+            <h3>Life Now:</h3>
+            <p>
+              I'm still living in NYC where I am working hard to continue to
+              improve and grow as a programmer, coding everyday, and loving
+              every learning moment.
+            </p>
+            <p>Live, Love, Code</p>
+          </div>
+        </div>
+        <div className="about-tech">
+          <h3>Technologies</h3>
+          <ul>
+            <li>JavaScript</li>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>SWR</li>
+            <li>Formik</li>
+            <li>Typescript</li>
+            <li>Flow</li>
+            <li>Bootstrap</li>
+            <li>Semantic UI</li>
+            <li>Material UI</li>
+            <li>Storybook</li>
+            <li>Ruby</li>
+            <li>Ruby on Rails</li>
+            <li>Python</li>
+            <li>Pytest</li>
+            <li>Django</li>
+            <li>Git</li>
+            <li>Documentation</li>
+            <li>Debugging</li>
+            <li>Cypress</li>
+            <li>Jest</li>
+          </ul>
         </div>
       </div>
     );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Link as LinkRouter } from "react-router-dom";
 import { Link, animateScroll as scroll } from "react-scroll";
-import ResponsiveMenu from "react-responsive-navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
@@ -10,17 +9,31 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
 
 library.add(faBars, faTimes);
 
 // need to remove react router
-// home will be on the far left
-// all others will be right aligned
-// figure out how I want mobile to look
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: "#cbb3c9",
+  },
+  drawer: {
+    width: 240,
+  },
+  drawerPaper: {
+    width: 240,
+    background: "#cbb3c9",
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-start",
   },
 }));
 
@@ -40,12 +53,18 @@ function ElevationScroll(props) {
 function NavBar(props) {
   const classes = useStyles();
 
+  const [openNav, setOpenNav] = useState(false);
+
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
 
   const scrollToBottom = () => {
     scroll.scrollToBottom();
+  };
+
+  const closeNav = () => {
+    setOpenNav(false);
   };
 
   const nav = () => {
@@ -63,11 +82,14 @@ function NavBar(props) {
           <li className="nav-item">
             <Link
               activeClass="active"
-              onClick={scrollToTop}
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
+              onClick={() => {
+                scrollToTop();
+                closeNav();
+              }}
             >
               Home
             </Link>
@@ -81,6 +103,7 @@ function NavBar(props) {
               smooth={true}
               offset={-70}
               duration={500}
+              onClick={closeNav}
             >
               Projects
             </Link>
@@ -94,6 +117,7 @@ function NavBar(props) {
               smooth={true}
               offset={-70}
               duration={500}
+              onClick={closeNav}
             >
               About
             </Link>
@@ -107,6 +131,7 @@ function NavBar(props) {
               smooth={true}
               offset={-70}
               duration={500}
+              onClick={closeNav}
             >
               Resume
             </Link>
@@ -120,6 +145,7 @@ function NavBar(props) {
               smooth={true}
               offset={-70}
               duration={500}
+              onClick={closeNav}
             >
               Blog
             </Link>
@@ -128,11 +154,14 @@ function NavBar(props) {
           <li className="nav-item">
             <Link
               activeClass="active"
-              onClick={scrollToBottom}
               spy={true}
               smooth={true}
               offset={-70}
               duration={500}
+              onClick={() => {
+                scrollToBottom();
+                closeNav();
+              }}
             >
               Contact
             </Link>
@@ -145,20 +174,39 @@ function NavBar(props) {
   return (
     <ElevationScroll {...props}>
       <AppBar className={classes.root}>
-        <Toolbar>{nav()}</Toolbar>
+        <div className="desktop-navbar">
+          <Toolbar>{nav()}</Toolbar>
+        </div>
+        <div className="responsive-drawer">
+          <IconButton
+            aria-label="open drawer"
+            edge="start"
+            onClick={() => setOpenNav(true)}
+          >
+            <FontAwesomeIcon icon={faBars} size="2x" />
+          </IconButton>
+          <Drawer
+            className={classes.drawer}
+            anchor="right"
+            open={openNav}
+            onClose={() => setOpenNav(false)}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.drawerHeader}>
+              <IconButton
+                aria-label="close drawer"
+                onClick={() => setOpenNav(false)}
+              >
+                <FontAwesomeIcon icon={faTimes} size="2x" />
+              </IconButton>
+            </div>
+            <List>{nav()}</List>
+          </Drawer>
+        </div>
       </AppBar>
     </ElevationScroll>
   );
 }
 export default NavBar;
-
-{
-  /* <ResponsiveMenu
-menuOpenButton={<FontAwesomeIcon icon={faBars} size="3x" />}
-menuCloseButton={<FontAwesomeIcon icon={faTimes} size="3x" />}
-changeMenuOn="500px"
-largeMenuClassName="large-menu-classname"
-smallMenuClassName="small-menu-classname"
-menu={nav()}
-/> */
-}
